a420cb2065548872ea1ab34beac43e4b2e9a18ec
alexponi <alexponi@mail.ru>
2013-04-04 17:59:37 +0600

    add migration user_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404100414) do
+ActiveRecord::Schema.define(:version => 20130404115831) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130404100414) do
     t.integer  "vote"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
