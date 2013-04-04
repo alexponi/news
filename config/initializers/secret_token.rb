3987c68c41af28d0391c5e55645ac66fd26a36a1
alexponi <alexponi@mail.ru>
2013-04-04 14:48:40 +0600

    init + model Posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+News::Application.config.secret_token = 'e66f862df16fad67d40b8ec36aebeacd24aca1105976ff49b672ec883dca3a6a438f737cadd449ce1022d00a13cd71633525fe2bce60f091c1cc022d1093d623'
